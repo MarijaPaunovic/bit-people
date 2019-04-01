084f81b2e2a8e7ffaa240fa12684b6923adb0957
BitStudent <student@bit.rs>
2019-04-01 17:55:42 +0200

    Features-4

--- src/app/users/Header.js
+++ src/app/users/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UsersPageCard from '../users/UserPageCard';
 
 const HeaderPost = () => (
     <>
@@ -6,11 +7,12 @@ const HeaderPost = () => (
             <div className="nav-wrapper">
                 <h1 className="brand-logo center">BIT Persons</h1>
 
-                <ul className="right hide-on-med-and-down">
+                <ul className="right">
 
                     <li><a href="index.html"><i className="fas fa-redo-alt"></i></a></li>
-                    <li><a href="#"><i className="fas fa-grip-horizontal"></i></a></li>
-
+                    {/* <li><a href="#"><i className="fas fa-grip-horizontal"></i></a></li> */}
+                    <li><a href="#" ><i className="fas fa-th-list"></i></a></li>
+                    {/* <input type=" text" onChange={this.handlerChange} /> */}
                 </ul>
             </div>
 
